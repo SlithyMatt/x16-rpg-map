e34aed42782ba74056ee4d4ca6e698c876432c09
Matt Heffernan <slithymatt@hotmail.com>
2020-06-10 02:34:57 -0400

    Created new 2-layer map with Tiled

---
+++ gfx/tiles8_128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="tiles8_128" tilewidth="8" tileheight="8" tilecount="128" columns="1">
+ <image source="tiles8_128.png" width="8" height="1024"/>
+</tileset>
